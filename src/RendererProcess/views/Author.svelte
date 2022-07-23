a9d718f9cc906ae3d1d129a882b0c054ebd5a118
joshnwosu <joshuanwosu078@gmail.com>
2022-07-23 23:18:23 +0100

    update

--- src/RendererProcess/views/Author.svelte
+++ src/RendererProcess/views/Author.svelte
@@ -1,9 +1,9 @@
 <script>
     export let params = {}
-    import {fade, fly, slide, scale} from "svelte/transition"
+    import {fade, fly, scale, slide, crossfade, draw} from "svelte/transition"
     
 </script>
 
-<main class="page" transition:slide>
+<main class="page" transition:fade>
     <h1>{params.first} {params.last}</h1>
 </main>
