190952ea4670abc5f15fedf6564d67ec2aa5fb27
joshnwosu <joshuanwosu078@gmail.com>
2022-07-23 23:15:00 +0100

    update

---
+++ src/RendererProcess/components/Root/ControlPanel.svelte
@@ -0,0 +1,13 @@
+<div class="control-panel"></div>
+
+<style>
+    .control-panel {
+        position: fixed;
+        /* border: 1px solid green; */
+        width: 100vw;
+        height: 80px;
+        bottom: 0;
+        background-color: rgba(68, 68, 68, 0.8);
+        z-index: 1000;
+    }
+</style>
