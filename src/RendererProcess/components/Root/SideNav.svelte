46f8354768b1eaca79a03c537f2ba2852e98cf63
joshnwosu <joshuanwosu078@gmail.com>
2022-07-23 22:28:41 +0100

    update

---
+++ src/RendererProcess/components/Root/SideNav.svelte
@@ -0,0 +1,21 @@
+<script>
+      import Router, {link, location, querystring} from "svelte-spa-router";
+</script>
+<div>
+    <nav>
+      <a href="/" use:link>Home</a>
+      <a href="/about" use:link>About</a>
+      <a href="/contact" use:link>Contact</a>
+      <a href="/author/Joshua/Nwosu" use:link>Author</a>
+      <a href="/book?search=joshua" use:link>Book</a>
+    </nav>
+</div>
+
+<style lang="scss">
+  nav {
+    position: relative;
+    a {
+      font-size: 20px ;
+    }
+  }
+</style>
