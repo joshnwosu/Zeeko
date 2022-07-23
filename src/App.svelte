7d97b7f2b94bd2ebeeb1c003b823f1092334227c
joshnwosu <joshuanwosu078@gmail.com>
2022-07-23 23:16:59 +0100

    update

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,8 @@
   import { routes } from "./RendererProcess/router";
 
   import CustomTitleBar from "./RendererProcess/components/Root/CustomTitleBar.svelte";
+  import SideNav from "./RendererProcess/components/Root/SideNav.svelte";
+  import ControlPanel from "./RendererProcess/components/Root/ControlPanel.svelte";
 
   const routeLoaded = (event) => {
     console.log('The Event:', event);
@@ -20,12 +22,16 @@
 <div id="app" on:click={cleanUp} on:contextmenu={cleanUp}>
   <CustomTitleBar />
 
-  <!-- <p>Location: {$location}</p> -->
   <!-- <p>Query String: {$querystring}</p> -->
 
-  <div class="main-view">
-     <!-- <Router {routes} on:routeLoaded={routeLoaded} /> -->
-  </div>
+  <section class="main-view">
+    <SideNav />
+    <div class="router-container">
+      <!-- <p>Location: {$location}</p> -->
+      <Router {routes} on:routeLoaded={routeLoaded} />
+    </div>
+    <ControlPanel />
+  </section>
  
 </div>
 
@@ -33,16 +39,36 @@
   :global(main.page) {
     position: absolute;
     width: 100%;
-    /* background: #000000 !important; */
+    height: 100%;
+    /* border: 1px solid red; */
+    padding: 20px;
+    flex: 1;
+    padding: 30px 20px 0px 20px;
+    background: #000000 !important;
   }
 
   .main-view {
-    background-color: transparent;
+    /* background-color: transparent; */
     position: relative;
-    width: 100vw;
-    height: 100vh;
+    /* width: 100vw; */
+    /* height: 100vh; */
     /* border: 1px solid red; */
-    background: #000000
+    /* background: #000000; */
+    width: 100%;
+    display: flex;
+    /* border: 1px solid red; */
+    height: 100%
+  }
+
+  .router-container {
+    /* border: 1px solid blue; */
+    position: relative;
+    /* display: flex; */
+    /* flex: 1; */
+    width: 100%;
+    /* height: 100%; */
+    /* padding: 30px 20px 0px 20px; */
+    
   }
   #app {
     height: 100vh;
@@ -50,6 +76,7 @@
     flex-direction: column;
     /* background: rgba(34, 34, 34, 1); */
     color: #FFFFFF;
+    background-color: #000000;
   }
   @media (min-width: 640px) {
     #app {
