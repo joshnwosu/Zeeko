72d8ebb548e5578ea855e8ed3ba2567aa20ef7d1
joshnwosu <joshuanwosu078@gmail.com>
2022-07-24 23:06:57 +0100

    Included some routes

--- src/RendererProcess/router/index.js
+++ src/RendererProcess/router/index.js
@@ -3,6 +3,7 @@ import RecentPlays from "../screens/RecentPlays.svelte";
 import NowPlaying from "../screens/NowPlaying.svelte";
 import Playlists from "../screens/Playlists.svelte";
 import Favorite from "../screens/Favorite.svelte";
+import Settings from "../screens/Settings.svelte";
 import NotFound from "../views/NotFound.svelte";
 
 import About from "../views/About.svelte";
@@ -16,6 +17,7 @@ export const routes = {
   "/now-playing": NowPlaying,
   "/playlists": Playlists,
   "/favorite": Favorite,
+  "/settings": Settings,
   // "/music/*": About,
   "*": NotFound,
 };
