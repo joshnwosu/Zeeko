d171f24e40a88f4465d00136ea9ae3d760e336d7
joshnwosu <joshuanwosu078@gmail.com>
2022-07-24 18:39:21 +0100

    update

--- src/RendererProcess/components/Root/ControlPanel.svelte
+++ src/RendererProcess/components/Root/ControlPanel.svelte
@@ -7,7 +7,8 @@
         width: 100vw;
         height: 80px;
         bottom: 0;
-        background-color: rgba(68, 68, 68, 0.8);
+        background-color: rgba(17, 17, 17, 0.9);
+        backdrop-filter: blur(20px);
         z-index: 1000;
     }
 </style>
