2aec870965126aa0874f8e51cbac007cfa106190
joshnwosu <joshuanwosu078@gmail.com>
2022-07-24 23:06:19 +0100

    Fixed route active class and some clean-up ��‍♀️

--- src/RendererProcess/components/Root/SideNav.svelte
+++ src/RendererProcess/components/Root/SideNav.svelte
@@ -1,6 +1,6 @@
 <script>
     import Router, {link, location, querystring} from "svelte-spa-router";
-import Favorite from "../Icons/Favorite.Icon.svelte";
+    import Favorite from "../Icons/Favorite.Icon.svelte";
     import MyMusic from "../Icons/MyMusic.Icon.svelte";
     import NowPlaying from "../Icons/NowPlaying.Icon.svelte";
     import Play from "../Icons/Play.Icon.svelte";
@@ -9,119 +9,147 @@ import Favorite from "../Icons/Favorite.Icon.svelte";
 
     import Icon from "svelte-icons-pack/Icon.svelte"
     import plusIcon from "svelte-icons-pack/vsc/VscAdd";
+    import Settings from "../Icons/Settings.Icon.svelte";
 
+    import active from 'svelte-spa-router/active'
 
-    const navigations = [
-        {
-            label: 'My music',
-            route: '/my-music/',
-            icon: MyMusic
-        },
-        {
-            label: 'Recent plays',
-            route: '/recent-plays',
-            icon: RecentPlays
-        },
-        {
-            label: 'Now playing',
-            route: '/now-playing',
-            icon: NowPlaying
-        },
-        {
-            label: 'Favorite',
-            route: '/favorite',
-            icon: Favorite
-        },
-        {
-            label: 'Playlists',
-            route: '/playlists',
-            icon: Playlists
-        }
-    ]
     
 </script>
 <div class="side-nav">
     <nav>
-        {#each navigations as navigation }
-        <div class="link">
-            <a href={navigation.route} use:link class={$location.indexOf(navigation.route) > -1 ? "active" : ""}>
-                <span class="icon">
-                    <svelte:component this={navigation.icon} />
-                </span>
-                <span class="label">
-                    {navigation.label}
-                </span>
-            </a>
-            
-            {#if navigation.label == 'Playlists' }
+        <div>
+            <!-- {#each navigations as navigation } -->
+            <div class="link">
+                <a href={'/my-music/'} use:link use:active={{path: '/my-music/*', className: 'active', inactiveClassName: ''}} 
+                >
+                    <span class="icon">
+                        <svelte:component this={MyMusic} />
+                    </span>
+                    <span class="label">
+                        My music
+                    </span>
+                </a>            
+            </div>
+            <div class="link">
+                <a href={'/recent-plays'} use:link use:active={{className: 'active', inactiveClassName: ''}} 
+                >
+                    <span class="icon">
+                        <svelte:component this={RecentPlays} />
+                    </span>
+                    <span class="label">
+                        Recent plays
+                    </span>
+                </a>            
+            </div>
+            <div class="link">
+                <a href={'/now-playing'} use:link use:active={{className: 'active', inactiveClassName: ''}} 
+                >
+                    <span class="icon">
+                        <svelte:component this={NowPlaying} />
+                    </span>
+                    <span class="label">
+                        Now playing
+                    </span>
+                </a>            
+            </div>
+            <div class="link">
+                <a href={'/favorite'} use:link use:active={{className: 'active', inactiveClassName: ''}} 
+                >
+                    <span class="icon">
+                        <svelte:component this={Favorite} />
+                    </span>
+                    <span class="label">
+                        Favorite
+                    </span>
+                </a>            
+            </div>
+            <!-- {/each} -->
+            <div class="line"><span /></div>
+            <div class="link">
+                <a href='/playlists' use:link use:active={{className: 'active', inactiveClassName: ''}}>
+                    <span class="icon">
+                        <svelte:component this={Playlists} />
+                    </span>
+                    <span class="label">
+                        Playlists
+                    </span>
+                </a>
+                
                 <div class="title-action plus-icon" on:click={() => console.log('plus icon')}>
                     <Icon src={plusIcon} color="#FFFFFF" size={14} />
                 </div>
-            {/if}
-            
+                
+            </div>
         </div>
-        {/each}
+        <div>
+            <div class="line"><span /></div>
+            <div class="link">
+                <a href='/settings' use:link use:active={{className: 'active', inactiveClassName: ''}}>
+                    <span class="icon">
+                        <svelte:component this={Settings} />
+                    </span>
+                    <span class="label">
+                        Settings
+                    </span>
+                </a>
+            </div>
+        </div>
     </nav>
 </div>
 
 <style lang="scss">
+    .line {
+        width: 100%;
+        padding: 0 10px;
+        span {
+            display: block;
+            width: 100%;
+            height: 1px;
+            background-color: #333;
+        }
+    }
     .side-nav {
         width: 300px;
         height: calc(100vh - 80px); // 150px is the height of the player-control
         background-color: #111111;
-        padding-top: 50px;
+        background-color: rgba(17, 17, 17, 0.9);
+        padding-top: 100px;
+        padding-bottom: 10px;
         /* padding-left: 20px; */
     }
-  nav {
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    .link {
-        display: flex;
-        /* align-items: center; */
-        /* flex: 1; */
-        /* border: 1px solid blue; */
-        height: 50px;
-    }
-
-    /* .plus-icon {
-        height: 50px;
-    } */
-
-    .title-action {
-        /* background-color: transparent; */
+    nav {
         position: relative;
-        width: 50px;
-        /* height: 100%; */
         display: flex;
-        /* flex: 1; */
-        justify-content: center;
+        flex-direction: column;
+        justify-content: space-between;
+        height: 100%;
+        .link {
+            display: flex;
+            height: 50px;
+        }
+        .title-action {
+            position: relative;
+            width: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            cursor: pointer;
+        }
+        .title-action:hover:not(.close-icon) {
+            background-color: #333333;
+        }
+        a {
+        font-size: 14px ;
+        padding: 10px 20px;
+        display: flex;
         align-items: center;
-        cursor: pointer;
-        /* z-index: 999; */
-    }
-
-    .title-action:hover:not(.close-icon) {
-        background-color: #333333;
-    }
-
-    a {
-      font-size: 14px ;
-      padding: 10px 20px;
-      display: flex;
-      align-items: center;
-      color: #FFFFFF;
-      border-left: 4px solid transparent;
-      flex: 1;
-      /* height: 50px; */
-      
-      &:hover {
-        background-color: rgba(68, 68, 68, .5);
-      }
-
-      &.active {
-        border-left: 4px solid rgb(68, 68, 68);
-      }
+        color: #FFFFFF;
+        border-left: 4px solid transparent;
+        flex: 1;
+        
+        &:hover {
+            background-color: rgba(68, 68, 68, .5);
+        }
 
       .icon {
         align-items: center;
