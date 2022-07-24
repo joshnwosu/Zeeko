f7d26932a91eef46bfe3438bd4015a44d3cb88df
joshnwosu <joshuanwosu078@gmail.com>
2022-07-24 23:01:44 +0100

    Added settings screen

---
+++ src/RendererProcess/screens/Settings.svelte
@@ -0,0 +1,36 @@
+<script>
+    import { fade, fly, scale, slide, crossfade, draw } from "svelte/transition"
+    import ScreenHeader from "../components/Root/ScreenHeader.svelte";
+    import { Tabs, TabList, TabPanel, Tab } from '../components/MusicTab';
+</script>
+<main class="page" transition:fade>
+    <ScreenHeader name="Settings" />
+    <div class="screen-view">
+        <p>Settings</p>
+
+        <Tabs>
+            <TabList>
+                <Tab>Songs</Tab>
+                <Tab>Artists</Tab>
+                <Tab>Albums</Tab>
+                <Tab>Folders</Tab>
+            </TabList>
+        
+            <TabPanel>
+                <h2>First panel</h2>
+            </TabPanel>
+        
+            <TabPanel>
+                <h2>Second panel</h2>
+            </TabPanel>
+        
+            <TabPanel>
+                <h2>Third panel</h2>
+            </TabPanel>
+
+            <TabPanel>
+                <h2>Forth panel</h2>
+            </TabPanel>
+        </Tabs>
+    </div>
+</main>
