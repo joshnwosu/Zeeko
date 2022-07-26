8150d5a9bac839502af8bb3a2f69d1ccf5d1657a
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 23:00:07 +0100

    Add Icon

---
+++ src/RendererProcess/components/Icons/Plus.Icon.svelte
@@ -0,0 +1,7 @@
+<svg
+    class="svg-ico"
+    xmlns="http://www.w3.org/2000/svg"
+    width="12"
+    height="12"
+    viewBox="0 0 24 24"><path fill="#FFFFFF"
+        d="M24 10h-10v-10h-4v10h-10v4h10v10h4v-10h10z" /></svg>
