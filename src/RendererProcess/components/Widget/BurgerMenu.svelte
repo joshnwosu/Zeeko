6893d90db34db8a83fb5d5cf9430cea2a5bc6c47
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 23:01:47 +0100

    update

---
+++ src/RendererProcess/components/Widget/BurgerMenu.svelte
@@ -0,0 +1,30 @@
+<div class="burger-menu">
+    <span></span>
+    <span></span>
+    <span></span>
+</div>
+
+<style>
+    .burger-menu {
+        width: 25px;
+        /* height: 20px; */
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-end;
+        
+    }
+
+    .burger-menu span {
+        display: block;
+        width: 100%;
+        height: 2px;
+        background-color: #FFFFFF;
+        margin: 2px 0;
+  
+    }
+
+    .burger-menu span:nth-child(2) {
+        width: 50%;        
+    }
+</style>
