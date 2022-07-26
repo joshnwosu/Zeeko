c505e8b56046740eb74ba0c141a71219533979f3
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 22:56:58 +0100

    electron preload 🏹🏹

---
+++ preload.js
@@ -0,0 +1,11 @@
+const { contextBridge, ipcRenderer } = require("electron");
+
+contextBridge.exposeInMainWorld("api", {
+  // we can also expose variables, not just functions
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  titlebar: (action) => {
+    ipcRenderer.send("titlebar", action);
+  },
+});
