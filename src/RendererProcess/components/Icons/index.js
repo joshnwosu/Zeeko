fcb1d7d6dd0a084e8e85dfe63d940ea040da8979
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 23:00:54 +0100

    icon index

---
+++ src/RendererProcess/components/Icons/index.js
@@ -0,0 +1,21 @@
+import PlayIcon from "./Play.svelte";
+import PauseIcon from "./PauseIcon.svelte";
+import ArrowLeftIcon from "./ArrowLeft.svelte";
+import ArrowRightIcon from "./ArrowRight.svelte";
+import SettingIcon from "./Setting.svelte";
+import SunnyIcon from "./Sunny.svelte";
+import HeartIcon from "./Heart.svelte";
+import MusicIcon from "./Music.svelte";
+import MusicFilterIcon from "./MusicFilter.svelte";
+
+export {
+  PlayIcon,
+  PauseIcon,
+  ArrowLeftIcon,
+  ArrowRightIcon,
+  SettingIcon,
+  SunnyIcon,
+  HeartIcon,
+  MusicIcon,
+  MusicFilterIcon,
+};
