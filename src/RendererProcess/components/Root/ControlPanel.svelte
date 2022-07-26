89d2a03ea7d9e143b1786bcaa425c4a437c8abab
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 22:54:27 +0100

    update

--- src/RendererProcess/components/Root/ControlPanel.svelte
+++ src/RendererProcess/components/Root/ControlPanel.svelte
@@ -1,14 +1,21 @@
-<div class="control-panel"></div>
+<script>
+  import { PlayIcon, PauseIcon } from "../../components/Icons";
+</script>
+
+<div class="control-panel">
+  <svelte:component this={PlayIcon} />
+  <svelte:component this={PauseIcon} />
+</div>
 
 <style>
-    .control-panel {
-        position: fixed;
-        /* border: 1px solid green; */
-        width: 100vw;
-        height: 80px;
-        bottom: 0;
-        background-color: rgba(17, 17, 17, 0.9);
-        backdrop-filter: blur(20px);
-        z-index: 1000;
-    }
+  .control-panel {
+    position: fixed;
+    /* border: 1px solid green; */
+    width: 100vw;
+    height: 80px;
+    bottom: 0;
+    background-color: rgba(17, 17, 17, 0.9);
+    backdrop-filter: blur(50px);
+    z-index: 1000;
+  }
 </style>
