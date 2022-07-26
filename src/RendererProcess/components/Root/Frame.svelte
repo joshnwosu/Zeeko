026943e3b3369131b108c6fe32cbc86981f1ca4a
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 23:01:26 +0100

    update

---
+++ src/RendererProcess/components/Root/Frame.svelte
@@ -0,0 +1,18 @@
+<div class="frame" />
+
+<style>
+  .frame {
+    width: 100%;
+    height: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0;
+    right: 0;
+    top: 0;
+    -webkit-app-region: drag;
+    position: fixed;
+    z-index: 999;
+    background-color: transparent;
+  }
+</style>
