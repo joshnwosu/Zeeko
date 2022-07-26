7a03389a327e4b411f5f74c3673cf158f5c2f706
joshnwosu <joshuanwosu078@gmail.com>
2022-07-26 23:01:19 +0100

    update

---
+++ src/RendererProcess/components/Root/BG.svelte
@@ -0,0 +1,31 @@
+<script>
+  const src = "./assets/images/431311.jpg";
+</script>
+
+<div class="bg">
+  <img {src} alt={src} />
+</div>
+
+<style>
+  .bg {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    filter: blur(20px);
+    pointer-events: none;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    opacity: 0.3;
+    position: fixed;
+    display: none;
+  }
+</style>
