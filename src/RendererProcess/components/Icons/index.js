1f3af639d80186cc8bcd5b6cf5b50f6a4572c42b
joshnwosu <joshuanwosu078@gmail.com>
2022-07-27 23:33:21 +0100

    update

--- src/RendererProcess/components/Icons/index.js
+++ src/RendererProcess/components/Icons/index.js
@@ -1,16 +1,52 @@
-import PlayIcon from "./Play.svelte";
-import PauseIcon from "./PauseIcon.svelte";
-import ArrowLeftIcon from "./ArrowLeft.svelte";
-import ArrowRightIcon from "./ArrowRight.svelte";
-import SettingIcon from "./Setting.svelte";
-import SunnyIcon from "./Sunny.svelte";
-import HeartIcon from "./Heart.svelte";
-import MusicIcon from "./Music.svelte";
-import MusicFilterIcon from "./MusicFilter.svelte";
+// outline icons import
+import PlayIcon from "./outline/Play.svelte";
+import PauseIcon from "./outline/Pause.svelte";
+import ArrowLeftIcon from "./outline/ArrowLeft.svelte";
+import ArrowRightIcon from "./outline/ArrowRight.svelte";
+import SettingIcon from "./outline/Setting.svelte";
+import SunnyIcon from "./outline/Sunny.svelte";
+import HeartIcon from "./outline/Heart.svelte";
+import MusicIcon from "./outline/Music.svelte";
+import MusicFilterIcon from "./outline/MusicFilter.svelte";
+import MusicDashboardIcon from "./outline/MusicDashboard.svelte";
+import MicrophoneIcon from "./outline/Microphone.svelte";
+import FolderIcon from "./outline/Folder.svelte";
+import SearchIcon from "./outline/Search.svelte";
+import HomeIcon from "./outline/Home.svelte";
+import MusicPlaylistIcon from "./outline/MusicPlaylist.svelte";
+import ClockIcon from "./outline/Clock.svelte";
+import NoteIcon from "./outline/Note.svelte";
+import NextIcon from "./outline/Next.svelte";
+import PreviousIcon from "./outline/Previous.svelte";
+import BackwardIcon from "./outline/Backward.svelte";
+import ForwardIcon from "./outline/Forward.svelte";
+import MoreIcon from "./outline/More.svelte";
+import MoreTwoIcon from "./outline/MoreTwo.svelte";
+
+// bold icons import
+import HomeBoldIcon from "./bold/HomeBold.svelte";
+import MusicBoldIcon from "./bold/MusicBold.svelte";
+import PlayBoldIcon from "./bold/Play.svelte";
+import PauseBoldIcon from "./bold/Pause.svelte";
+import NextBoldIcon from "./bold/Next.svelte";
+import PreviousBoldIcon from "./bold/Previous.svelte";
+import BackwardBoldIcon from "./bold/Backward.svelte";
+import ForwardBoldIcon from "./bold/Forward.svelte";
+import ShuffleBoldIcon from "./bold/Shuffle.svelte";
+import RepeatBoldIcon from "./bold/Repeat.svelte";
+import RepeatOneBoldIcon from "./bold/RepeatOne.svelte";
+import HeartBoldIcon from "./bold/Heart.svelte";
+import MusicFilterBoldIcon from "./bold/MusicFilter.svelte";
+import MenuBoldIcon from "./bold/Menu.svelte";
 
 export {
+  // outline icons
   PlayIcon,
   PauseIcon,
+  NextIcon,
+  PreviousIcon,
+  BackwardIcon,
+  ForwardIcon,
   ArrowLeftIcon,
   ArrowRightIcon,
   SettingIcon,
@@ -18,4 +54,30 @@ export {
   HeartIcon,
   MusicIcon,
   MusicFilterIcon,
+  MusicDashboardIcon,
+  MicrophoneIcon,
+  FolderIcon,
+  SearchIcon,
+  HomeIcon,
+  MusicPlaylistIcon,
+  ClockIcon,
+  NoteIcon,
+  MoreIcon,
+  MoreTwoIcon,
+
+  // bold icons
+  HomeBoldIcon,
+  MusicBoldIcon,
+  PlayBoldIcon,
+  PauseBoldIcon,
+  NextBoldIcon,
+  PreviousBoldIcon,
+  BackwardBoldIcon,
+  ForwardBoldIcon,
+  ShuffleBoldIcon,
+  RepeatBoldIcon,
+  RepeatOneBoldIcon,
+  HeartBoldIcon,
+  MusicFilterBoldIcon,
+  MenuBoldIcon,
 };
