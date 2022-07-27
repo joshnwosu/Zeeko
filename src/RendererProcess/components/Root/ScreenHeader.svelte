2071d451027cf5e8249f04c3e96a93760cf8eb6b
joshnwosu <joshuanwosu078@gmail.com>
2022-07-27 23:34:35 +0100

    update

--- src/RendererProcess/components/Root/ScreenHeader.svelte
+++ src/RendererProcess/components/Root/ScreenHeader.svelte
@@ -1,35 +1,33 @@
 <script>
-    export let name;
-    // const formatRoute  = (str) => {
-    //     return str.toLowerCase().replace(/[/]/g, '').replace(/[-]/, ' ').replace(/\b\w/i, c => c.toUpperCase())
-    // }
+  export let name;
+  // const formatRoute  = (str) => {
+  //     return str.toLowerCase().replace(/[/]/g, '').replace(/[-]/, ' ').replace(/\b\w/i, c => c.toUpperCase())
+  // }
 </script>
 
 <div class="screen-header">
-    <h1>{name}</h1>
-    <slot />
+  <h1>{name}</h1>
+  <slot />
 </div>
 
 <style lang="scss">
-    .screen-header {
-        position: sticky;
-        top: 0;
-        right: 0;
-        left: 0;
-        width: 100%;
-        min-height: 150px;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-end;
-        background-color: rgba(0, 0, 0, 0.8);
-        backdrop-filter: blur(20px);
-        padding: 0px 20px 20px;
-        border-bottom: 1px solid #222222;
-
-        
-        h1 {
-            font-size: 30px;
-            font-weight: 200;
-        }
+  .screen-header {
+    position: sticky;
+    top: 0;
+    right: 0;
+    left: 0;
+    width: 100%;
+    min-height: 150px;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    background-color: rgba(14, 18, 26, 0.8);
+    backdrop-filter: blur(20px);
+    padding: 0px 20px 20px;
+    border-bottom: 1px solid #171c26;
+    h1 {
+      font-size: 30px;
+      font-weight: 200;
     }
+  }
 </style>
