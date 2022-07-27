c6138692c56724961ac207fed5e6cb681033a93e
joshnwosu <joshuanwosu078@gmail.com>
2022-07-27 23:34:47 +0100

    update

--- src/RendererProcess/components/Root/SideBar.svelte
+++ src/RendererProcess/components/Root/SideBar.svelte
@@ -1,37 +1,163 @@
 <script>
-  import { pop } from "svelte-spa-router";
-  import { ArrowLeftIcon, ArrowRightIcon } from "../Icons";
+  let search = "";
+  import {
+    ArrowLeftIcon,
+    ArrowRightIcon,
+    SearchIcon,
+    HomeIcon,
+    HeartIcon,
+    MusicFilterIcon,
+    MusicIcon,
+    MusicPlaylistIcon,
+    ClockIcon,
+    NoteIcon,
+    HomeBoldIcon,
+    MusicBoldIcon,
+  } from "../Icons";
+  import { link, location, pop, querystring } from "svelte-spa-router";
+  import active from "svelte-spa-router/active";
 </script>
 
 <div class="side-nav">
-  <div class="action-wrapper">
-    <div class="window-action-button">
-      <span on:click={() => window?.api?.titlebar("closeWindow")} />
-      <span on:click={() => window?.api?.titlebar("minimize")} />
-      <span on:click={() => window?.api?.titlebar("maximize")} />
+  <div>
+    <div class="action-wrapper">
+      <div class="window-action-button">
+        <span on:click={() => window?.api?.titlebar("closeWindow")} />
+        <span on:click={() => window?.api?.titlebar("minimize")} />
+        <span on:click={() => window?.api?.titlebar("maximize")} />
+      </div>
+
+      <div class="navigation-action-button">
+        <span on:click={pop}>
+          <svelte:component this={ArrowLeftIcon} />
+        </span>
+        <span>
+          <svelte:component this={ArrowRightIcon} />
+        </span>
+      </div>
     </div>
 
-    <div class="navigation-action-button">
-      <span on:click={pop}>
-        <svelte:component this={ArrowLeftIcon} />
-      </span>
-      <span>
-        <svelte:component this={ArrowRightIcon} />
-      </span>
+    <div class="logo">
+      <div class="logo-inner">
+        <p>Z</p>
+      </div>
+      <h3>eeko music</h3>
     </div>
+
+    <div class="search-bar">
+      <span class="icon">
+        <svelte:component this={SearchIcon} />
+      </span>
+      <input bind:value={search} placeholder="Search" />
+    </div>
+  </div>
+
+  <div class="scroll-nav">
+    <nav>
+      <ul>
+        <div class="ul-head">Menu</div>
+        <li class={$location == "/" && "active-link-li"}>
+          <a
+            href={"/"}
+            use:link
+            use:active={{ className: "active-link", inactiveClassName: "" }}
+          >
+            <span class="icon">
+              <svelte:component this={HomeIcon} />
+            </span>
+            <span class="label"> Discover </span>
+          </a>
+        </li>
+        <li class={$location == "/my-music/" && "active-link-li"}>
+          <a
+            href={"/my-music/"}
+            use:link
+            use:active={{
+              path: "/my-music/*",
+              className: "active-link",
+              inactiveClassName: "",
+            }}
+          >
+            <span class="icon">
+              <!-- <svelte:component
+                this={$location == "/my-music/" ? MusicIcon : MusicBoldIcon}
+              /> -->
+              <svelte:component this={MusicIcon} />
+            </span>
+            <span class="label"> My music </span>
+          </a>
+        </li>
+        <li class={$location == "/recent-plays" && "active-link-li"}>
+          <a
+            href={"/recent-plays"}
+            use:link
+            use:active={{ className: "active-link", inactiveClassName: "" }}
+          >
+            <span class="icon">
+              <svelte:component this={ClockIcon} />
+            </span>
+            <span class="label"> Recent plays </span>
+            <!-- <span class="label">你好吗 我很好</span> -->
+          </a>
+        </li>
+        <li class={$location == "/now-playing" && "active-link-li"}>
+          <a
+            href={"/now-playing"}
+            use:link
+            use:active={{ className: "active-link", inactiveClassName: "" }}
+          >
+            <span class="icon">
+              <svelte:component this={NoteIcon} />
+            </span>
+            <span class="label"> Now playing </span>
+          </a>
+        </li>
+      </ul>
+
+      <ul>
+        <div class="ul-head">Playlists</div>
+        <!-- <li class={$location == "/favorite" && "active-link-li"}>
+          <a
+            href={"/favorite"}
+            use:link
+            use:active={{ className: "active-link", inactiveClassName: "" }}
+          >
+            <span class="icon">
+              <svelte:component this={HeartIcon} />
+            </span>
+            <span class="label"> Favorite </span>
+          </a>
+        </li> -->
+
+        <li class={$location == "/playlists" && "active-link-li"}>
+          <a
+            href={"/playlists"}
+            use:link
+            use:active={{ className: "active-link", inactiveClassName: "" }}
+          >
+            <span class="icon">
+              <svelte:component this={MusicPlaylistIcon} />
+            </span>
+            <span class="label"> Playlists </span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   </div>
 </div>
 
 <style lang="scss">
   .side-nav {
     background-color: rgba(14, 18, 26, 0.7);
-    background-color: rgba(23, 28, 38, 0.7);
+    background-color: rgba(23, 28, 38, 0.8);
     width: 300px;
     position: fixed;
     top: 0;
     left: 0;
     bottom: 0;
-    padding: 20px 0;
+    padding: 20px 0 0;
+    display: flex;
+    flex-direction: column;
   }
 
   .action-wrapper {
@@ -90,5 +216,159 @@
   .navigation-action-button span:nth-child(2) {
     opacity: 0.5;
     pointer-events: none;
+  }
+
+  .logo {
+    padding: 40px 20px;
+    display: flex;
+    align-items: center;
+    display: none;
+    .logo-inner {
+      width: 50px;
+      height: 50px;
+      border-radius: 50%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #65e14d;
+      margin-right: 5px;
+      p {
+        font-family: "Harlow Solid";
+        font-size: 30px;
+        font-weight: 900;
+        color: rgba(23, 28, 38, 0.9);
+      }
+    }
+    h3 {
+      font-family: "Operator mono";
+      font-size: 20px;
+      font-weight: 400;
+    }
+  }
+
+  .search-bar {
+    margin: 20px 10px 0 10px;
+    overflow: hidden;
+    position: relative;
+    border-radius: 10px;
+    input {
+      background-color: rgba(14, 18, 26, 0.5);
+      width: 100%;
+      height: 50px;
+      padding: 0px 50px 0px 60px;
+      border: none;
+      color: #ffffff;
+      font-size: 16px;
+    }
+    .icon {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 60px;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+    }
+  }
+
+  .scroll-nav {
+    overflow-y: auto;
+    flex: 1;
+    &::-webkit-scrollbar-track-piece:end {
+      background: transparent;
+      margin-bottom: 0px;
+    }
+  }
+  nav {
+    ul {
+      margin-top: 20px;
+      .ul-head {
+        font-size: 12px;
+        font-weight: 600;
+        color: #ffffff;
+        opacity: 0.5;
+        padding-left: 30px;
+        margin-bottom: 10px;
+        text-transform: uppercase;
+      }
+      li {
+        padding: 0px 10px;
+        position: relative;
+        &.active-link-li::after {
+          content: "";
+          position: absolute;
+          width: 4px;
+          height: 30px;
+          background-color: #65e14d !important;
+          top: 50%;
+          right: 0;
+          transform: translate(-50%, -50%);
+          border-top-left-radius: 5px;
+          border-bottom-left-radius: 5px;
+        }
+        a {
+          font-size: 12px;
+          font-weight: 400;
+          display: flex;
+          align-items: center;
+          padding: 15px 10px;
+          border-radius: 10px;
+          &:hover {
+            /* background-color: rgba(14, 18, 26, 0.2); */
+            background-image: linear-gradient(
+              90deg,
+              rgba(14, 18, 26, 0.2),
+              transparent
+            );
+            .icon {
+              :global(.svg-icon-bold) {
+                :global(path) {
+                  fill: #ffffff;
+                }
+              }
+              :global(.svg-icon-outline) {
+                :global(path) {
+                  stroke: #ffffff;
+                }
+              }
+            }
+          }
+
+          &.active-link {
+            .label {
+              color: #65e14d !important;
+            }
+            .icon {
+              :global(.svg-icon-bold) {
+                :global(path) {
+                  fill: #65e14d !important;
+                }
+              }
+              :global(.svg-icon-outline) {
+                :global(path) {
+                  stroke: #65e14d !important;
+                }
+              }
+            }
+          }
+          .label {
+            color: #ffffff;
+          }
+          .icon {
+            width: 50px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-right: 10px;
+            :global(svg) {
+              width: 20px;
+              height: 20px;
+            }
+          }
+        }
+      }
+    }
   }
 </style>
