24db67d19bd1547188d9f74b272c70757a79dd77
joshnwosu <joshuanwosu078@gmail.com>
2022-07-27 23:35:19 +0100

    update

--- src/RendererProcess/screens/NowPlaying.svelte
+++ src/RendererProcess/screens/NowPlaying.svelte
@@ -1,19 +1,14 @@
 <script>
-    import { fade, fly, scale, slide, crossfade, draw } from "svelte/transition"
+  import { fade, fly, scale, slide, crossfade, draw } from "svelte/transition";
 </script>
-<main class="page">
-    <div class="now-playing">
 
-    </div>
+<main class="page">
+  <div class="now-playing" />
 </main>
 
 <style>
-
-    .now-playing {
-        width: 100%;
-        height: 100%;
-        bottom: 0;
-        background-color: #111111;
-    }
-
+  .now-playing {
+    width: 100%;
+    height: 100%;
+  }
 </style>
