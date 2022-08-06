68f5ca0f549f427028dde0bef706bc905d066157
joshnwosu <joshuanwosu078@gmail.com>
2022-08-06 19:55:15 +0100

    update

--- src/RendererProcess/screens/Playlists.svelte
+++ src/RendererProcess/screens/Playlists.svelte
@@ -2,15 +2,29 @@
   import { fade, fly, scale, slide, crossfade, draw } from "svelte/transition";
   import ScreenHeader from "../components/Root/ScreenHeader.svelte";
   import { playlistStore } from "../store";
+  // let albumArt = '';
+
+  function getFisrtAlbumArt(playlist) {
+    let v;
+    playlist.tracks.some((track) => (v = track.albumArt));
+    return v;
+  }
+
+  // $: $playlistStore.forEach(playlist => playlist.tracks.some(s => {
+  //   albumArt = s
+  // }))
 </script>
 
 <main class="page" transition:fade>
   <ScreenHeader title="Playlists" />
   <div class="screen-view">
-    <!-- <p>Playlists</p> -->
     {#each $playlistStore as playlist}
       <div class="playlist-card">
-        <div class="cover" />
+        <div class="cover">
+          {#if getFisrtAlbumArt(playlist)}
+            <img src={getFisrtAlbumArt(playlist)} alt={`album-art`} />
+          {/if}
+        </div>
         <div class="label">
           <p class="name">{playlist.name}</p>
           <p class="length">{playlist.tracks.length} Songs</p>
@@ -31,17 +45,21 @@
       height: 180px;
       background-color: #121212;
 
-      padding: 20px;
+      /* padding: 20px; */
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+      }
     }
     .label {
       margin-top: 10px;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 0 10px 0 0;
+      padding: 0 10px 0 10px;
 
       p {
         font-size: 14px;
+        margin-bottom: 5px;
         &.name {
           opacity: 1;
           font-weight: 400;
