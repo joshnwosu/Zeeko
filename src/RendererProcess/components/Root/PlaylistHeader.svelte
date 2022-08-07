856570958266c47e723eb66c9230a5cebd115df9
joshnwosu <joshuanwosu078@gmail.com>
2022-08-07 18:17:01 +0100

    update

---
+++ src/RendererProcess/components/Root/PlaylistHeader.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let playlist;
+  import { getFisrtAlbumArt } from "../../store/playbackManager";
+  import BlurImg from "../Widget/BlurImg.svelte";
+  import Cover from "../Widget/Cover.svelte";
+</script>
+
+<div class="header">
+  <BlurImg img={getFisrtAlbumArt(playlist)} />
+  <div class="header-content">
+    <Cover img={getFisrtAlbumArt(playlist)} />
+    <div class="details">
+      <h1>{playlist?.name}</h1>
+      <p>{playlist?.tracks?.length} Songs</p>
+    </div>
+  </div>
+</div>
+
+<style lang="scss">
+  .header {
+    width: 100%;
+    background-color: #121212;
+    position: relative;
+    .header-content {
+      width: 100%;
+      display: flex;
+      position: relative;
+      padding: 50px 20px;
+      z-index: 3;
+      .details {
+        padding: 20px;
+        h1 {
+          font-weight: 100;
+          font-size: 30px;
+        }
+
+        p {
+          font-size: 12px;
+          margin-top: 10px;
+          opacity: 0.6;
+        }
+      }
+    }
+  }
+</style>
--- src/RendererProcess/views/PlaylistDetails.svelte
+++ src/RendererProcess/views/PlaylistDetails.svelte
@@ -1,12 +1,10 @@
 <script>
   export let params;
   import { fade, fly, scale, slide, crossfade, draw } from "svelte/transition";
+  import PlaylistHeader from "../components/Root/PlaylistHeader.svelte";
   import PlaylistView from "../components/Root/PlaylistView.svelte";
   import TrackLists from "../components/Track/TrackLists.svelte";
-  import BlurImg from "../components/Widget/BlurImg.svelte";
-  import Cover from "../components/Widget/Cover.svelte";
   import { playlistStore } from "../store";
-  import { getFisrtAlbumArt } from "../store/playbackManager";
   let playlist;
 
   $: (function getPlaylist(payload) {
@@ -18,49 +16,9 @@
   })(params.name);
 </script>
 
-<main class="page playlist-details" transition:fade>
-  <div class="header">
-    <BlurImg img={getFisrtAlbumArt(playlist)} />
-    <div class="header-content">
-      <Cover img={getFisrtAlbumArt(playlist)} />
-      <div class="details">
-        <h1>{params.name}</h1>
-        <p>{playlist?.tracks?.length} Songs</p>
-      </div>
-    </div>
-  </div>
-
+<main class="page" transition:fade>
+  <PlaylistHeader {playlist} />
   <PlaylistView>
-    {#if params?.name}
-      <TrackLists tracks={playlist?.tracks} />
-    {/if}
+    <TrackLists tracks={playlist?.tracks} />
   </PlaylistView>
 </main>
-
-<style lang="scss">
-  .header {
-    width: 100%;
-    background-color: #121212;
-    position: relative;
-    .header-content {
-      width: 100%;
-      display: flex;
-      position: relative;
-      padding: 50px 20px;
-      z-index: 3;
-      .details {
-        padding: 20px;
-        h1 {
-          font-weight: 100;
-          font-size: 30px;
-        }
-
-        p {
-          font-size: 12px;
-          margin-top: 10px;
-          opacity: 0.6;
-        }
-      }
-    }
-  }
-</style>
