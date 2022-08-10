7b812e9cdb98c91dde7b4de8a6d7634594c89e30
joshnwosu <joshuanwosu078@gmail.com>
2022-08-10 14:36:38 +0100

    update

--- src/RendererProcess/components/Layouts/Appearance.svelte
+++ src/RendererProcess/components/Layouts/Appearance.svelte
@@ -1,16 +1,50 @@
 <script>
+  import { accentColorsConfig } from "../../config/accentColors";
   import Switch from "../Addon/Switch.svelte";
   let checked1 = false;
-  let checked2 = false;
 </script>
 
-<div>
-  <div>
-    <h1>Music on this PC</h1>
+<div class="appearance">
+  <div class="section accent-color-section">
+    <h1>Choose your accent color</h1>
+    <div class="accent-color">
+      {#each accentColorsConfig as color}
+        <span style="background-color: {color}" class="color" />
+      {/each}
+    </div>
   </div>
 
-  <h2>First panel</h2>
-  <p>{checked1} {checked2}</p>
-  <Switch checked={checked1} toggle={(e) => (checked1 = e.target.checked)} />
-  <Switch checked={checked2} toggle={(e) => (checked2 = e.target.checked)} />
+  <div class="section theme-section">
+    <h1>Choose your theme</h1>
+  </div>
+
+  <div class="system">
+    <p>Use system preference</p>
+    <Switch checked={checked1} toggle={(e) => (checked1 = e.target.checked)} />
+  </div>
 </div>
+
+<style lang="scss">
+  .appearance {
+    .section {
+      h1 {
+        font-weight: 400;
+        font-size: 18px;
+      }
+
+      .accent-color {
+        display: flex;
+        align-items: center;
+        .color {
+          width: 30px;
+          height: 30px;
+          border-radius: 50%;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          margin-right: 10px;
+        }
+      }
+    }
+  }
+</style>
