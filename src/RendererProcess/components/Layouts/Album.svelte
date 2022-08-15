64af1b70b06024eb6f29368917431e4842d156a8
joshnwosu <joshuanwosu078@gmail.com>
2022-08-15 12:27:40 +0100

    update

--- src/RendererProcess/components/Layouts/Album.svelte
+++ src/RendererProcess/components/Layouts/Album.svelte
@@ -1,11 +1,13 @@
 <script>
+  import { push } from "svelte-spa-router";
+
   export let data;
   import { defaultCoverArt } from "../../utilities";
 </script>
 
 <div class="card-container">
   {#each data as album}
-    <div class="card">
+    <div class="card" on:click={() => push(`#/artist-details/${album.artist}`)}>
       <img src={album?.tracks[0]?.albumArt || defaultCoverArt} alt="album" />
       <div class="content">
         <p class="name">{album.name}</p>
@@ -23,18 +25,21 @@
 
     .card {
       width: 200px;
-      /* &:hover {
-          background-color: #121212;
-        } */
+      background-color: #121212;
+      border-radius: 5px;
+      overflow: hidden;
+      &:hover {
+        background-color: #333333;
+      }
       img {
-        width: 200px;
+        width: 100%;
         height: 160px;
         object-fit: cover;
         object-position: center;
       }
 
       .content {
-        /* padding: 10px; */
+        padding: 10px 10px;
         P {
           font-size: 12px;
           color: #ffffff;
