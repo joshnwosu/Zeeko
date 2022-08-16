caaa5ad22aa56a4da3b83fd9232f1fcf4d8d07fc
joshnwosu <joshuanwosu078@gmail.com>
2022-08-16 03:02:06 +0100

    update

--- src/RendererProcess/views/ArtistDetails.svelte
+++ src/RendererProcess/views/ArtistDetails.svelte
@@ -1,6 +1,7 @@
 <script>
   export let params;
   import { fade, fly, scale, slide, crossfade, draw } from "svelte/transition";
+  import { UserIcon } from "../components/Icons";
   import PlaylistHeader from "../components/Playlist/PlaylistHeader.svelte";
   import PlaylistView from "../components/Playlist/PlaylistView.svelte";
   import TrackLists from "../components/Track/TrackLists.svelte";
@@ -15,7 +16,16 @@
 <main class="page" transition:fade>
   {#if $artistsStore.length}
     <div class="header">
-      <h1>{params.name}</h1>
+      <div class="figure">
+        <svelte:component this={UserIcon} />
+      </div>
+      <div class="header-right">
+        <div class="text">
+          <h1>{params.name}</h1>
+          <p>Albums &#x2022; {artistData?.albums.length}</p>
+          <p>Tracks &#x2022; {artistData?.tracks.length}</p>
+        </div>
+      </div>
     </div>
 
     <h1 class="title">Albums</h1>
@@ -40,7 +50,9 @@
 
     <h1 class="title">Tracks</h1>
     {#if artistData?.tracks}
-      <TrackLists tracks={artistData?.tracks} />
+      <PlaylistView>
+        <TrackLists tracks={artistData?.tracks} />
+      </PlaylistView>
     {/if}
   {/if}
 </main>
@@ -51,7 +63,7 @@
       font-size: 18px;
       font-weight: 200;
       color: #ffffff;
-      padding: 10px;
+      padding: 20px;
     }
   }
   .container {
@@ -98,5 +110,35 @@
     position: sticky;
     top: 0;
     z-index: 2;
+    display: flex;
+    align-items: center;
+    gap: 20px;
+    padding: 0 20px;
+    .figure {
+      width: 100px;
+      height: 100px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      background-color: #121212;
+      border-radius: 50%;
+
+      :global(svg) {
+        width: 50px;
+        height: 50px;
+        opacity: 0.5;
+      }
+    }
+
+    .header-right {
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .text {
+        p {
+          font-size: 12px;
+        }
+      }
+    }
   }
 </style>
