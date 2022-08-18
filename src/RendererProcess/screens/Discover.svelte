72482fe45bb0b3babd503471bab7d31be3866acc
joshnwosu <joshuanwosu078@gmail.com>
2022-08-18 20:26:45 +0100

    update

--- src/RendererProcess/screens/Discover.svelte
+++ src/RendererProcess/screens/Discover.svelte
@@ -1,17 +1,16 @@
 <script>
   import ScreenHeader from "../components/Root/ScreenHeader.svelte";
-  import GenreTags from "../components/Widgets/GenreTags.svelte";
-  import LibraryStats from "../components/Widgets/LibraryStats.svelte";
-  import Slidy from "../components/Widgets/Slidy.svelte";
+  // import LibraryStats from "../components/Widgets/LibraryStats.svelte";
+  // import GenreTags from "../components/Widgets/GenreTags.svelte";
+  // import Slidy from "../components/Widgets/Slidy.svelte";
 </script>
 
 <main class="page">
   <ScreenHeader title="Discover" />
   <div class="screen-view">
     <!-- <Slidy /> -->
-    <LibraryStats />
-    <GenreTags />
-    <div class="height" />
+    <!-- <LibraryStats /> -->
+    <!-- <GenreTags /> -->
   </div>
 </main>
 
@@ -19,11 +18,5 @@
   .screen-view {
     width: 1100px;
     max-width: 100%;
-  }
-
-  .height {
-    width: 300px;
-    height: 1000px;
-    background-color: red;
   }
 </style>
