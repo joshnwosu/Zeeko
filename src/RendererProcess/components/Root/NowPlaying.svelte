e4539b74f3c5c8ce78748a37e91bd7ff69d6c519
joshnwosu <joshuanwosu078@gmail.com>
2022-09-21 22:29:11 +0100

    update

--- src/RendererProcess/components/Root/NowPlaying.svelte
+++ src/RendererProcess/components/Root/NowPlaying.svelte
@@ -1,8 +1,29 @@
 <script>
   import { playbackManager, queuelistStore } from "../../store/player";
+  import {
+    nextSong,
+    prevSong,
+    repeatSong,
+    shuffleSong,
+    stepBackward,
+    stepForward,
+    togglePlaying,
+  } from "../../store/playerManager";
   import { handleRouting } from "../../store/statusManager";
   import { defaultCoverArt } from "../../utilities";
+  import {
+    Backward10Icon,
+    Forward10Icon,
+    NextBoldIcon,
+    PauseBoldIcon,
+    PlayBoldIcon,
+    PreviousBoldIcon,
+    RepeatBoldIcon,
+    RepeatOneBoldIcon,
+    ShuffleBoldIcon,
+  } from "../Icons";
   import Track from "../Track/Track.svelte";
+  import TrackBar from "../Widgets/TrackBar.svelte";
 </script>
 
 <div class="now-playing">
@@ -39,6 +60,53 @@
         </div>
       </div>
     </div>
+    <div class="control-track-bar">
+      <TrackBar />
+    </div>
+    <div class="control-btn">
+      <span
+        class="icon repeat-icon right-flare"
+        class:repeat-on={$playbackManager?.repeat == 1 ||
+          $playbackManager?.repeat == 2}
+        on:click={repeatSong}
+      >
+        {#if $playbackManager?.repeat == 0}
+          <svelte:component this={RepeatBoldIcon} />
+        {:else if $playbackManager?.repeat == 1}
+          <svelte:component this={RepeatBoldIcon} />
+        {:else if $playbackManager?.repeat == 2}
+          <svelte:component this={RepeatOneBoldIcon} />
+        {/if}
+      </span>
+      <span class="icon previous-icon left-flare" on:click={prevSong}>
+        <svelte:component this={PreviousBoldIcon} />
+      </span>
+      <span class="icon backward-icon left-flare" on:click={stepBackward}>
+        <svelte:component this={Backward10Icon} />
+      </span>
+      <div class="play-pause" on:click={togglePlaying}>
+        <span class="icon play-pause-icon">
+          {#if $playbackManager.playing}
+            <svelte:component this={PauseBoldIcon} />
+          {:else}
+            <svelte:component this={PlayBoldIcon} />
+          {/if}
+        </span>
+      </div>
+      <span class="icon forward-icon right-flare" on:click={stepForward}>
+        <svelte:component this={Forward10Icon} />
+      </span>
+      <span class="icon next-icon right-flare" on:click={nextSong}>
+        <svelte:component this={NextBoldIcon} />
+      </span>
+      <span
+        class="icon shuffle-icon right-flare"
+        class:shuffle-on={$playbackManager?.shuffle}
+        on:click={shuffleSong}
+      >
+        <svelte:component this={ShuffleBoldIcon} />
+      </span>
+    </div>
   </div>
   <div class="track-list">
     <Track tracks={$queuelistStore} />
@@ -57,6 +125,74 @@
       height: calc(100% - 100px);
       overflow-y: auto;
       margin-top: 20px;
+    }
+  }
+
+  .control-track-bar {
+    margin-top: 20px;
+  }
+
+  .control-btn {
+    margin: 10px 0 20px 0;
+    display: flex;
+    /* justify-content: space-between; */
+    /* justify-content: center; */
+    align-items: center;
+    gap: 20px;
+  }
+
+  .play-pause {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 4px;
+    background-color: rgba(255, 255, 255, 0.1);
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.2);
+    }
+  }
+
+  .icon {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    cursor: default;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+
+    :global(svg) {
+      width: 25px;
+      height: 25px;
+    }
+    &.left-flare:hover {
+      background-image: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0.1),
+        transparent
+      );
+    }
+    &.right-flare:hover {
+      background-image: linear-gradient(
+        90deg,
+        transparent,
+        rgba(255, 255, 255, 0.1)
+      );
+    }
+    &:hover:not(.play-pause-icon) {
+      :global(svg.svg-icon-bold) {
+        :global(path) {
+          fill: #ffffff;
+        }
+      }
+      :global(svg.svg-icon-outline) {
+        :global(path) {
+          stroke: #ffffff;
+        }
+      }
     }
   }
 
